366a035e4c3fb782b28d69b4cbb15de8550ff122
lol3rrr <s.loler03@gmail.com>
2019-09-14 18:27:56 +0200

    Moved the comparisons of the paths when searching for a custom route to its own function for easier modification later on

--- src/webserver/server/callCustomPath.c
+++ src/webserver/server/callCustomPath.c
@@ -1,9 +1,25 @@
 #include "../server.h"
 
+int comparePath(char* givenPath, char* requestedPath) {
+  int givenLength = getStringLength(givenPath);
+  int requestedLength = getStringLength(requestedPath);
+  int difference = givenLength - requestedLength;
+  if (difference != 0)
+    return -1;
+
+  int length = givenLength;
+  for (int i = 0; i < length; i++) {
+    if (givenPath[i] != requestedPath[i])
+      return -1;
+  }
+
+  return 0;
+}
+
 int findPathNode(pathNode_t* head, char* method, char* path, pathNode_t** result) {
   pathNode_t* current = head;
   while (current != NULL) {
-    if (strcmp(current->method, method) == 0 && strcmp(current->path, path) == 0) {
+    if (strcmp(current->method, method) == 0 && comparePath(path, current->path) == 0) {
       (*result) = current;
       return 0;
     }
