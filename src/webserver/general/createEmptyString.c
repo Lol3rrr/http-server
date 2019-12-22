a822c2bd53973d9a475596d2e5eac5d36351b656
lol3rrr <s.loler03@gmail.com>
2019-12-22 11:33:35 +0100

    CreateEmptyString now also uses createEmptyCString to create the content

--- src/webserver/general/createEmptyString.c
+++ src/webserver/general/createEmptyString.c
@@ -1,11 +1,8 @@
 #include "../headerFiles/general.h"
 
 string* createEmptyString(int length) {
-  char* content = (char*) malloc((length + 1) * sizeof(char));
-  content[length] = '\0';
-
   string* result = (string*) malloc(1 * sizeof(string));
-  result->content = content;
+  result->content = createEmptyCString(length);
   result->length = length;
 
   return result;
