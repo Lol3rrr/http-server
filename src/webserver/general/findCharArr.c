5076e9b73f9a1a1f27fc3e0905ae4ec95c694238
lol3rrr <s.loler03@gmail.com>
2020-02-14 13:20:36 +0100

    Made another small change to the comparison in the findCharArr function

--- src/webserver/general/findCharArr.c
+++ src/webserver/general/findCharArr.c
@@ -7,17 +7,17 @@ int checkForKey(char* src, char* key, int i, int keyLength) {
     }
   }
 
-  return i;
+  return 0;
 }
 
 int findCharArr(char* src, char* key, int srcLength, int keyLength) {
   for (int i = 0; i < srcLength - keyLength; i++) {
     int result = checkForKey(src, key, i, keyLength);
-    if (result < 0) {
+    if (result != 0) {
       continue;
     }
 
-    return result;
+    return i;
   }
 
   return -1;
