25194cc6d9177a0b199745ed5d9cc4537423a39f
lol3rrr <s.loler03@gmail.com>
2020-04-22 21:06:22 +0200

    Made a small change to the way the createHTTPHeaderPart function works, which should make it faster

--- src/webserver/response/createHTTPHeaderPart.c
+++ src/webserver/response/createHTTPHeaderPart.c
@@ -1,5 +1,16 @@
 #include "../headerFiles/response.h"
 
+static int getHeaderPartLength(kvNode_t* node, int spacerLength) {
+  int length = getKVPairLength(node);
+  length += spacerLength;
+
+  if (node->next) {
+    length += getHeaderPartLength(node->next, spacerLength);
+  }
+
+  return length;
+}
+
 int createHTTPHeaderPart(response* respPtr, char* spacer, int spacerLength, char** result) {
   int headerLength = -1;
   char* headerPart;
@@ -9,42 +20,25 @@ int createHTTPHeaderPart(response* respPtr, char* spacer, int spacerLength, char
     kvNode_t* currentKV = headers->kvNodes;
     headerPartNode_t* head = NULL;
 
-    headerLength = 0;
+    headerLength = getHeaderPartLength(headers->kvNodes, spacerLength);
+    headerPart = createEmptyCString(headerLength);
+
+    char* buffer = headerPart;
 
     while (currentKV != NULL) {
       string result;
       createKVPair(currentKV, &result);
-      headerLength += result.length;
-      headerLength += spacerLength;
 
+      memcpy(buffer, result.content, result.length);
+      buffer += result.length;
 
-      if (head == NULL) {
-        head = (headerPartNode_t*) malloc(1 * sizeof(headerPartNode_t));
-        head->content = result.content;
-        head->length = result.length;
-        head->next = NULL;
-      }else {
-        pushHeaderNodePart(head, result.content, result.length);
-      }
+      memcpy(buffer, spacer, spacerLength);
+      buffer += spacerLength;
+
+      free(result.content);
 
       currentKV = currentKV->next;
     }
-
-    headerPart = createEmptyCString(headerLength);
-    int offset = 0;
-
-    headerPartNode_t* current = head;
-    while (current != NULL) {
-      memcpy(headerPart + offset, current->content, current->length);
-      offset += current->length;
-
-      memcpy(headerPart + offset, spacer, spacerLength);
-      offset += spacerLength;
-
-      current = current->next;
-    }
-
-    cleanHeaderNodePart(head);
 
     *result = headerPart;
   }
