4f6a445769037dcc556b35c87be1926d226548f8
lol3rrr <s.loler03@gmail.com>
2020-05-02 15:00:34 +0200

    Made a couple of optimizations to the new binary Header Tree

--- src/webserver/binaryHeaderTree/cmp.c
+++ src/webserver/binaryHeaderTree/cmp.c
@@ -1,7 +1,5 @@
 #include "../headerFiles/binaryHeaderTree.h"
 
 int cmp(string keyStr, int keyHash, BTreeNode_t* node) {
-  return (keyStr.length != node->key.length) ? 
-    keyStr.length - node->key.length :
-    (keyHash != node->keyHash) ? keyHash - node->keyHash : strcmp(keyStr.content, node->key.content);
+  return (keyHash != node->keyHash) ? keyHash - node->keyHash : strcmp(keyStr.content, node->key.content);
 }
