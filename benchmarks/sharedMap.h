5b512f2565fde6489a6fb9910a3169d69785c82f
lol3rrr <s.loler03@gmail.com>
2020-06-14 14:57:33 +0200

    Added more benchmarks

--- benchmarks/sharedMap.h
+++ benchmarks/sharedMap.h
@@ -10,10 +10,33 @@
 
 #include "general.h"
 
-void calculateHashBench();
+void BM_BucketIndex(benchmark::State& state) {
+  int size = state.range(0);
+  
+  unsigned int seed = time(NULL);
+  for (auto _ : state) {
+    state.PauseTiming();
+    unsigned int hash = rand_r(&seed);
+    state.ResumeTiming();
 
-long int benchCalculateHash(char* str);
+    bucketIndex(size, hash);
+  }
+}
 
-void getBucketIndexBench();
+void BM_CalculateHash(benchmark::State& state) {
+  int size = state.range(0);
+  char* line = (char*) malloc(size * sizeof(char));
+  
+  unsigned int seed = time(NULL);
+  for (int i = 0; i < size; i++) {
+    line[i] = rand_r(&seed);
+  } 
+
+  for (auto _ : state) {
+    calcHash(line);
+  }
+
+  free(line);
+}
 
 #endif
