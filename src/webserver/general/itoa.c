4696bb3dca283b85f1ea647bf1f4358c4520ab38
lol3rrr <s.loler03@gmail.com>
2020-06-15 02:43:16 +0200

    made a small optimization for the itoa function

--- src/webserver/general/itoa.c
+++ src/webserver/general/itoa.c
@@ -1,24 +1,38 @@
 #include "../headerFiles/general.h"
 
+static int digits10(int numb) {
+  int result = 1;
+  for (;;) {
+    if (numb < 10) return result;
+    if (numb < 100) return result + 1;
+    if (numb < 1000) return result + 2;
+    if (numb < 10000) return result + 3;
+
+    numb /= 10000;
+    result += 4;
+  }
+
+  return 0;
+}
+
 char* itoa(int i, char b[]){
-  char* p = b;
-  if(i<0){
-    *p++ = '-';
+  const int result = digits10(i);
+  int pos = result;
+  if(i < 0){
+    b[pos--] = '-';
     i *= -1;
   }
 
-  int shifter = i;
-  do{ //Move to where representation ends
-    ++p;
-    shifter = shifter/10;
-  }while(shifter);
+  while (i >= 10) {
+    const int q = i / 10;
+    const int r = i % 10;
+    b[pos--] = '0' + r;
+    i = q;
+  }
 
-  *p = '\0';
-
-  do{ //Move back, inserting digits as u go
-    *--p = digit[i%10];
-    i = i/10;
-  }while(i);
+  if (pos != 0) {
+    b[0] = '0' + i;
+  }
 
   return b;
 }
