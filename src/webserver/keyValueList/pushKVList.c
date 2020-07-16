92d91cc1c5246bce32656d4b46ce2096092ff55f
lol3rrr <s.loler03@gmail.com>
2020-07-16 15:07:19 +0200

    Updated some stuff

--- src/webserver/keyValueList/pushKVList.c
+++ src/webserver/keyValueList/pushKVList.c
@@ -4,13 +4,8 @@ kvNode_t* pushKVList(kvList_t* list, string key, string value) {
   if (list->currentIndex < list->bufferSize) {
     int index = list->currentIndex;
 
-    list->buffer[index].key.content = key.content;
-    list->buffer[index].key.length = key.length;
-    list->buffer[index].key.needsFree = key.needsFree;
-      
-    list->buffer[index].value.content = value.content;
-    list->buffer[index].value.length = value.length;
-    list->buffer[index].value.needsFree = value.needsFree;
+    memcpy(&(list->buffer[index].key), &key, sizeof(string));
+    memcpy(&(list->buffer[index].value), &value, sizeof(string));
 
     list->currentIndex++;
 
